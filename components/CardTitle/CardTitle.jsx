aa99f5555727006f4f2301df38f88e7ad3fa2ae1
AureLeeLi <aduynslaeger@gmail.com>
2024-11-05 19:44:27 +0100

    about page almosst done : google map, travel time, visit ?, social networks to come

---
+++ components/CardTitle/CardTitle.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CardTitle = ({ children }) => {
+  return (
+    <h3 className="mx-auto text-center text-3xl font-semibold">{children}</h3>
+  )
+}
+
+export default CardTitle
